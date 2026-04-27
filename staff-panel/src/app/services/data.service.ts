e6eb280708a7bfef1977696e13b0dbd018a032ff
Ruti <andrearutigliano76@gmail.com>
2026-04-27 14:58:23 +0000

    Staff: creato progetto Angular e DataService

---
+++ staff-panel/src/app/services/data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  // ATTENZIONE: Qui devi mettere l'indirizzo della tua Codespace Backend sulla porta 5000
+  // Lo trovi nella tab "Ports" vicino al terminale. Deve finire con .app.github.dev
+  private baseUrl = 'https://verbose-memory-v6446j5p57rpfwqxp-5000.app.github.dev'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Gestione Prodotti
+  getProdotti(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/prodotti`);
+  }
+
+  addProdotto(prodotto: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/prodotti`, prodotto);
+  }
+
+  eliminaProdotto(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/prodotti/${id}`);
+  }
+
+  // Gestione Ordini
+  getOrdini(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/ordini`);
+  }
+
+  aggiornaStatoOrdine(id: number, nuovoStato: string): Observable<any> {
+    return this.http.patch(`${this.baseUrl}/ordini/${id}`, { stato: nuovoStato });
+  }
+}
